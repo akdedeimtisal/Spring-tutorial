7559a2722145f9bfec8254fe89539b9b5b77fc04
akdedeimtisal <akdedeimtisall@gmail.com>
2022-02-26 19:35:53 +0300

    Java Configuration File add

---
+++ src/main/java/com/springdemoannotation/JavaConfigDemoApp.java
@@ -0,0 +1,33 @@
+package com.springdemoannotation;
+
+import java.io.FileNotFoundException;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class JavaConfigDemoApp {
+
+	public static void main(String[] args) throws FileNotFoundException, Exception {
+		
+		//read spring config java class
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+		
+		
+		//get the bean from spring container
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		
+	//	Coach golfCoach = context.getBean("golfCoach", Coach.class);
+		
+		//call a method on the bean
+		System.out.println(theCoach.getDailyWorkout());
+	//	System.out.println(golfCoach.getDailyWorkout());
+		
+		//call method to get the daily fortune
+		System.out.println(theCoach.getDailyFortune());
+	
+		
+		//close the context
+		context.close();
+	}
+
+}
